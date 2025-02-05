250dac567fcbbd84895e4f17cae1330ec5367b06
CormacRice <147517010+CormacRice@users.noreply.github.com>
2025-02-05 21:32:16 +0000

    Add files via upload

---
+++ Sockets/client.py
@@ -0,0 +1,12 @@
+import socket
+
+Host = '192.168.0.222'
+Port =  6999
+
+socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((Host, Port))
+
+socket.send("Hello World!".encode('ascii'))
+
+print(socket.receive(1024))
+
---
+++ Sockets/server.py
@@ -0,0 +1,22 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+Host = '192.168.0.222'
+Port = 6999
+
+server.bind((Host, Port))
+
+server.listen(2)
+
+while True:
+    communication_socket, address = server.accept()
+    print(f"Connection to {address}")
+    message = communication_socket.recv(1024).decode('ascii')
+    print(f'Message from client is {message}')
+    communication_socket.send(f"Message received, thank you!".encode('ascii'))
+    communication_socket.close()
+    print(f"Connection with {address} has ended!")
+
+    
+
