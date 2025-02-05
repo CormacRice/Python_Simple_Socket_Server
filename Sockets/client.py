ddb3b4cef8f585b0082a789f23fb4a8a7382cfa9
CormacRice <147517010+CormacRice@users.noreply.github.com>
2025-02-05 21:51:58 +0000

    Update client.py

--- Sockets/client.py
+++ Sockets/client.py
@@ -3,7 +3,7 @@ import socket
 Host = '192.168.0.222'
 Port =  6999
 
-socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((Host, Port))
 
 socket.send("Hello World!".encode('ascii'))
